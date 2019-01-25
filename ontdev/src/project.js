378552ff56b82e0a77de51791c51e9c7438a19a8
hsiaosiyuan0 <hsiaosiyuan0@outlook.com>
2019-01-25 12:04:24 +0800

    closes #12

--- ontdev/src/project.js
+++ ontdev/src/project.js
@@ -1,10 +1,10 @@
-const { Box } = require("punica-ts/lib/box/box");
+const {Box} = require("punica-ts/lib/box/box");
 const git = require("isomorphic-git");
 const fs = require("fs");
 const util = require("util");
 const tmp = require("tmp");
 const rimraf = require("rimraf");
-const { Compiler } = require("./compiler");
+const {Compiler} = require("./compiler");
 const copy = require("recursive-copy");
 
 const tmpDir = util.promisify(tmp.dir);
@@ -13,15 +13,33 @@ const rma = util.promisify(rimraf);
 git.plugins.set("fs", fs);
 
 module.exports.Project = class {
+  static async gitClone(repoUrl, repoToPath = '') {
+    console.log('Downloading...');
+    if (fs.existsSync(repoToPath)) {
+      if (fs.statSync(repoToPath).isDirectory()) {
+        if (fs.readdirSync(repoToPath).length > 0) {
+          throw new Error("File already exists");
+        }
+      } else {
+        throw new Error("File already exists");
+      }
+    }
+    try {
+      await git.clone({url: repoUrl, dir: repoToPath});
+    } catch (e) {
+      throw new Error("Network Error: " + e);
+    }
+  }
+
   static async init(dir) {
     try {
       await fs.promises.access(dir, fs.constants.R_OK | fs.constants.W_OK);
       const td = await tmpDir();
       console.log("Downloading into temporary directory: " + td);
-      await new Box().init(td);
+      await this.gitClone("https://gitee.com/punica-box/init-box", td);
 
       console.log("Copying into project directory...");
-      await copy(td, dir, { filter: ["**/*", "!.git"] });
+      await copy(td, dir, {filter: ["**/*", "!.git"]});
 
       console.log("Removing temporary directory...");
       await rma(td);
